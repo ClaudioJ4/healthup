45a6c4b750f1931fc68ac64f2d4c89a4f45fdfa8
Claudio Jose <cjdgc13@gmail.com>
2024-06-27 19:55:29 -0300

    About page + Updates

--- lib/features/auth/front/pages/about_page.dart
+++ lib/features/auth/front/pages/about_page.dart
@@ -1,57 +1,116 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
 
-import 'package:flutter/material.dart';
 import 'package:healthup/constants/front_constants.dart';
+import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
-  const AboutPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.backgroundColor,
       appBar: AppBar(
-        backgroundColor: AppColors.secondBackgroundColor,
         title: Text(
-          'Sobre o app',
+          'Sobre',
           style: TextStyle(color: Colors.white),
         ),
         iconTheme: IconThemeData(color: Colors.white),
-        automaticallyImplyLeading: true,
+        backgroundColor: AppColors.secondBackgroundColor,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: AppColors.secondBackgroundColor,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
-            children: [
-              Text(
-                'Health Up!',
-                style: TextStyle(
+      backgroundColor: AppColors.backgroundColor,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Nome do App',
+              style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20),
-              Text(
-                '   O HealthUp é um aplicativo desenvolvido para ajudá-lo a monitorar e gerenciar sua saúde. '
-                'Com este aplicativo, você pode registrar suas atividades, acompanhar seus hábitos de saúde e muito mais.',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.justify,
-              ),
-            ],
-          ),
+                  color: Colors.white),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Health UP!',
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Desenvolvedores',
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Cláudio Confessor\nPedro Eduardo\nOrientado por: Ceres Germano',
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Objetivo do Aplicativo',
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'O FitnessApp é projetado para ajudar os usuários a manterem uma vida fitness, fornecendo ferramentas para rastreamento de calorias, consumo de água e planejamento de atividades físicas.',
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 16),
+            _buildModuleSection(
+              context,
+              'Contador de Calorias',
+              'Este módulo permite ao usuário adicionar refeições e calorias consumidas diariamente. O progresso é mostrado através de uma barra de progresso com uma meta estipulada pelo usuário.',
+              'assets/caloriascount.png',
+            ),
+            _buildModuleSection(
+              context,
+              'Contador de Água',
+              'O usuário pode adicionar manualmente a quantidade de água ingerida diariamente, com uma barra de progresso e uma meta diária.',
+              'assets/aguacount.png',
+            ),
+            _buildModuleSection(
+              context,
+              'Calendário de Atividades',
+              'Permite ao usuário marcar atividades físicas em um calendário, identificando-as com cores e assinalando quando foram concluídas.',
+              'assets/calendario.png',
+            ),
+            _buildModuleSection(
+              context,
+              'Perfil',
+              'Exibe o nome de usuário, email e um gráfico anual mostrando o total de calorias e água ingeridas mês a mês.',
+              'assets/perfil.png',
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildModuleSection(BuildContext context, String title,
+      String description, String imagePath) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        SizedBox(height: 8),
+        Text(
+          description,
+          style: TextStyle(color: Colors.white),
+        ),
+        SizedBox(height: 16),
+        Center(
+          child: Image.asset(imagePath),
+        ),
+        SizedBox(height: 32),
+      ],
     );
   }
 }
